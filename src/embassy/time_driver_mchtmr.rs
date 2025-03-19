8cd3423345700f5e12bb9556b4170f2cf02a80c4
Haobo Gu <haobogu@outlook.com>
2025-03-19 18:19:05 +0800

    feat(time-driver): update to latest embassy-time-driver
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- src/embassy/time_driver_mchtmr.rs
+++ src/embassy/time_driver_mchtmr.rs
@@ -1,26 +1,21 @@
 //! Embassy time driver using machine timer(mchtmr)
 
-use core::cell::Cell;
-use core::sync::atomic::{AtomicU32, AtomicU8, Ordering};
-use core::{mem, ptr};
+use core::cell::{Cell, RefCell};
+use core::task::Waker;
 
 use critical_section::{CriticalSection, Mutex};
-use embassy_time_driver::AlarmHandle;
+use embassy_time_driver::{Driver, TICK_HZ};
+use embassy_time_queue_utils::Queue;
 use hpm_metapac::sysctl::vals;
 use hpm_metapac::{MCHTMR, SYSCTL};
+use portable_atomic::{AtomicU64, Ordering};
 
 use crate::sysctl::ClockConfig;
 use crate::{interrupt, pac};
 
-pub const ALARM_COUNT: usize = 1;
-
+// Alarm state structure to manage individual alarms
 struct AlarmState {
     timestamp: Cell<u64>,
-
-    // This is really a Option<(fn(*mut ()), *mut ())>
-    // but fn pointers aren't allowed in const yet
-    callback: Cell<*const ()>,
-    ctx: Cell<*mut ()>,
 }
 
 unsafe impl Send for AlarmState {}
@@ -29,26 +24,32 @@ impl AlarmState {
     const fn new() -> Self {
         Self {
             timestamp: Cell::new(u64::MAX),
-            callback: Cell::new(ptr::null()),
-            ctx: Cell::new(ptr::null_mut()),
         }
     }
 }
 
-pub struct MachineTimerDriver {
-    alarm_count: AtomicU8,
-    alarms: Mutex<[AlarmState; ALARM_COUNT]>,
-    period: AtomicU32,
+// Machine timer based time driver implementation
+pub(crate) struct MachineTimerDriver {
+    // Total number of ticks since system start
+    ticks: AtomicU64,
+    // Number of allocated alarms
+    alarm: Mutex<AlarmState>,
+    queue: Mutex<RefCell<Queue>>,
 }
 
+// Constant initialization for alarm states
+#[allow(clippy::declare_interior_mutable_const)]
 const ALARM_STATE_NEW: AlarmState = AlarmState::new();
+
+// Macro to create a static driver instance
 embassy_time_driver::time_driver_impl!(static DRIVER: MachineTimerDriver = MachineTimerDriver {
-    period: AtomicU32::new(1), // avoid div by zero
-    alarm_count: AtomicU8::new(0),
-    alarms: Mutex::new([ALARM_STATE_NEW; ALARM_COUNT]),
+    ticks: AtomicU64::new(0),
+    alarm: Mutex::new(ALARM_STATE_NEW),
+    queue: Mutex::new(RefCell::new(Queue::new()))
 });
 
 impl MachineTimerDriver {
+    // Initialize the machine timer driver
     fn init(&'static self) {
         // FIXME: The name in SDK is MCHTMR0
         #[cfg(hpm67)]
@@ -62,9 +63,6 @@ impl MachineTimerDriver {
         };
 
         let cnt_per_second = crate::sysctl::clocks().get_freq(&mchtmr_cfg).0 as u64;
-        let cnt_per_tick = cnt_per_second / embassy_time_driver::TICK_HZ;
-
-        self.period.store(cnt_per_tick as u32, Ordering::Relaxed);
 
         // make sure mchtmr will not be gated on "wfi"
         // Design consideration: use WAIT is also useful to enter low-power mode
@@ -87,103 +85,71 @@ impl MachineTimerDriver {
         MCHTMR.mtimecmp().write_value(u64::MAX - 1);
     }
 
-    #[inline(always)]
+    // Machine timer interrupt handler
     fn on_interrupt(&self) {
         unsafe {
             riscv::register::mie::clear_mtimer();
         }
 
         critical_section::with(|cs| {
-            self.trigger_alarm(cs);
+            // Increment global tick counter
+            let current_ticks = self.ticks.fetch_add(1, Ordering::Relaxed);
+            self.check_and_trigger_alarm(current_ticks, cs);
         })
     }
 
-    #[inline(always)]
-    fn trigger_alarm(&self, cs: CriticalSection) {
-        let alarm = &self.alarms.borrow(cs)[0];
-        alarm.timestamp.set(u64::MAX);
+    // Check if an alarm is due and trigger it if necessary
+    #[inline]
+    fn check_and_trigger_alarm(&self, current_time: u64, cs: CriticalSection) {
+        let alarm = &self.alarm.borrow(cs);
+        let alarm_timestamp = alarm.timestamp.get();
 
-        // Call after clearing alarm, so the callback can set another alarm.
-
-        // safety:
-        // - we can ignore the possiblity of `f` being unset (null) because of the safety contract of `allocate_alarm`.
-        // - other than that we only store valid function pointers into alarm.callback
-        let f: fn(*mut ()) = unsafe { mem::transmute(alarm.callback.get()) };
-        f(alarm.ctx.get());
-    }
-
-    fn get_alarm<'a>(&'a self, cs: CriticalSection<'a>, alarm: AlarmHandle) -> &'a AlarmState {
-        // safety: we're allowed to assume the AlarmState is created by us, and
-        // we never create one that's out of bounds.
-        unsafe { self.alarms.borrow(cs).get_unchecked(alarm.id() as usize) }
-    }
-}
-
-impl embassy_time_driver::Driver for MachineTimerDriver {
-    fn now(&self) -> u64 {
-        MCHTMR.mtime().read() / self.period.load(Ordering::Relaxed) as u64
-    }
-
-    unsafe fn allocate_alarm(&self) -> Option<AlarmHandle> {
-        let id = self.alarm_count.fetch_update(Ordering::AcqRel, Ordering::Acquire, |x| {
-            if x < ALARM_COUNT as u8 {
-                Some(x + 1)
-            } else {
-                None
+        // Check if alarm is scheduled and due
+        if alarm_timestamp != u64::MAX && current_time >= alarm_timestamp {
+            let mut next = self.queue.borrow(cs).borrow_mut().next_expiration(current_time);
+            while !self.set_alarm(cs, next) {
+                next = self.queue.borrow(cs).borrow_mut().next_expiration(self.now());
             }
-        });
-
-        match id {
-            Ok(id) => Some(AlarmHandle::new(id)),
-            Err(_) => None,
         }
     }
 
-    fn set_alarm_callback(&self, alarm: AlarmHandle, callback: fn(*mut ()), ctx: *mut ()) {
-        critical_section::with(|cs| {
-            let alarm = self.get_alarm(cs, alarm);
+    // Set alarm timestamp
+    fn set_alarm(&self, cs: CriticalSection, timestamp: u64) -> bool {
+        if self.now() >= timestamp {
+            // Alarm time has passed, cannot set
+            return false;
+        }
+        self.alarm.borrow(cs).timestamp.set(timestamp);
+        if self.now() >= timestamp {
+            self.alarm.borrow(cs).timestamp.set(u64::MAX);
+            return false;
+        }
+        true
+    }
+}
 
-            alarm.callback.set(callback as *const ());
-            alarm.ctx.set(ctx);
-        })
+// Implement the Driver trait for MachineTimerDriver
+impl Driver for MachineTimerDriver {
+    // Get current system time in ticks
+    fn now(&self) -> u64 {
+        self.ticks.load(Ordering::Relaxed)
     }
 
-    fn set_alarm(&self, alarm: AlarmHandle, timestamp: u64) -> bool {
+    fn schedule_wake(&self, at: u64, waker: &Waker) {
         critical_section::with(|cs| {
-            let _n = alarm.id();
+            let mut queue = self.queue.borrow(cs).borrow_mut();
 
-            let alarm = self.get_alarm(cs, alarm);
-            alarm.timestamp.set(timestamp);
-
-            let t = self.now();
-            if timestamp <= t {
-                // If alarm timestamp has passed the alarm will not fire.
-                // Disarm the alarm and return `false` to indicate that.
-                unsafe {
-                    riscv::register::mie::clear_mtimer();
+            if queue.schedule_wake(at, waker) {
+                let mut next = queue.next_expiration(self.now());
+                while !self.set_alarm(cs, next) {
+                    next = queue.next_expiration(self.now());
                 }
-
-                alarm.timestamp.set(u64::MAX);
-
-                return false;
             }
-
-            let safe_timestamp = timestamp
-                .saturating_add(1)
-                .overflowing_mul(self.period.load(Ordering::Relaxed) as u64)
-                .0;
-
-            MCHTMR.mtimecmp().write_value(safe_timestamp);
-            unsafe {
-                riscv::register::mie::set_mtimer();
-            }
-
-            true
         })
     }
 }
 
-// Core local interrupts are handled in CORE_LOCAL
+// Machine timer interrupt handler (to be implemented in your interrupt vector)
 #[interrupt]
 fn MachineTimer() {
     DRIVER.on_interrupt();
