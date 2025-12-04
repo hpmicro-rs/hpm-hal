2c8645354a26584062d94a503229ded62bc450bd
Andelf <andelf@gmail.com>
2025-12-04 21:16:09 +0800

    feat: add another embassy timer driver using gptmr

--- src/embassy/mod.rs
+++ src/embassy/mod.rs
@@ -1,12 +1,27 @@
 //! The embassy time driver for HPMicro MCUs.
 //!
-//! Dev Note: Unlike STM32, GPTMR(TMR) can not be used for time driver because it lacks of channel sychronization.
-//! See-also: https://github.com/hpmicro/hpm-hal/issues/9
+//! ## Time Driver Options
+//!
+//! - **MCHTMR (default)**: Uses 64-bit machine timer, most accurate
+//! - **GPTMR**: Uses 32-bit general purpose timer with period mechanism
+//!
+//! Enable `time-driver-gptmr0` or `time-driver-gptmr1` feature to use GPTMR instead.
 
+#[cfg(not(any(time_driver_gptmr0, time_driver_gptmr1)))]
 #[path = "time_driver_mchtmr.rs"]
+pub mod time_driver_impl;
+
+#[cfg(any(time_driver_gptmr0, time_driver_gptmr1))]
+#[path = "time_driver_gptmr.rs"]
 pub mod time_driver_impl;
 
 // This should be called after global clocks inited
 pub(crate) fn init() {
+    #[cfg(not(any(time_driver_gptmr0, time_driver_gptmr1)))]
     time_driver_impl::init();
+
+    #[cfg(any(time_driver_gptmr0, time_driver_gptmr1))]
+    critical_section::with(|cs| {
+        time_driver_impl::init(cs);
+    });
 }
